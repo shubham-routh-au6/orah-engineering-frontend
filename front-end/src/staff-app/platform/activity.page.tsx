c255d67700f0ee6370907bf9ea872c9392bb2a66
Shubh121 <shubham@cuttingchaitech.com>
2022-10-10 20:35:20 +0530

    fixed bug in activity page and removed console.log statement

--- front-end/src/staff-app/platform/activity.page.tsx
+++ front-end/src/staff-app/platform/activity.page.tsx
@@ -6,17 +6,18 @@ import { Colors } from "shared/styles/colors"
 import { Activity } from "shared/models/activity"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome/"
 import { faPlay } from "@fortawesome/free-solid-svg-icons"
-import { DetailsPage } from "./details.page"
+import { CenteredContainer } from "shared/components/centered-container/centered-container.component"
 
 export const ActivityPage: React.FC = () => {
-  const [getActivities, fetchedActivities, loader] = useApi<{ activity: Activity[] }>({ url: "get-activities" })
+  const [getActivities, fetchedActivities, loadState] = useApi<{ activity: Activity[] }>({ url: "get-activities" })
 
   const [data, setData] = useState<Activity[]>([])
   const [showDetails, setShowDetails] = useState<boolean>(false)
   const [index, setIndex] = useState<number>()
 
-  if (fetchedActivities && !data.length) {
-    console.log("this is hola", fetchedActivities)
+  if (fetchedActivities && fetchedActivities.activity.length && !data.length) {
+    console.log("fetchedActivities", fetchedActivities)
+    console.log("loadState", loadState)
     setData(fetchedActivities.activity)
   }
 
@@ -25,7 +26,6 @@ export const ActivityPage: React.FC = () => {
   }
 
   const handleDetails = (i: number) => {
-    console.log("clicked", index)
     setShowDetails(!showDetails)
 
     if (i === index) {
@@ -43,60 +43,77 @@ export const ActivityPage: React.FC = () => {
     <>
       <S.Container>
         <S.HeaderContainer>Activities</S.HeaderContainer>
-        <S.CardContainer>
-          {data.map((d, i) => {
-            return (
-              <S.Card key={i}>
-                {index !== i && (
-                  <>
-                    {" "}
-                    <S.CardItem>
-                      <S.CardItemSpan>Type: &nbsp;</S.CardItemSpan>
-                      {d.type}
-                    </S.CardItem>
-                    <S.CardItem>
-                      <S.CardItemSpan>Date: &nbsp;</S.CardItemSpan>
-                      {formatDate(d.date)}
-                    </S.CardItem>{" "}
-                  </>
-                )}
-                <S.CardItem>
-                  <S.CardItemSpan>Details: &nbsp;</S.CardItemSpan>
-                  <S.CardIconContainer>
-                    <FontAwesomeIcon onClick={() => handleDetails(i)} icon={faPlay} />
-                  </S.CardIconContainer>
-                  {showDetails && index === i && (
+        {loadState === "loading" && (
+          <CenteredContainer>
+            <FontAwesomeIcon icon="spinner" size="2x" spin />
+          </CenteredContainer>
+        )}
+        {loadState === "error" && (
+          <CenteredContainer>
+            <div>Failed to load</div>
+          </CenteredContainer>
+        )}
+        {loadState === "loaded" && !data.length && (
+          <CenteredContainer>
+            <div>No activities found</div>
+          </CenteredContainer>
+        )}
+        {loadState === "loaded" && data && (
+          <S.CardContainer>
+            {data.map((d, i) => {
+              return (
+                <S.Card key={i}>
+                  {index !== i && (
                     <>
-                      <S.EntityContainer>
-                        <S.CardItemSpan>Name: &nbsp;</S.CardItemSpan> {d.entity.name}
-                      </S.EntityContainer>
-                      <S.EntityContainer>
-                        <S.CardItemSpan>Completed at: &nbsp;</S.CardItemSpan> {formatDate(d.entity.completed_at)}
-                      </S.EntityContainer>
+                      {" "}
+                      <S.CardItem>
+                        <S.CardItemSpan>Type: &nbsp;</S.CardItemSpan>
+                        {d.type}
+                      </S.CardItem>
+                      <S.CardItem>
+                        <S.CardItemSpan>Date: &nbsp;</S.CardItemSpan>
+                        {formatDate(d.date)}
+                      </S.CardItem>{" "}
                     </>
                   )}
-                </S.CardItem>
-                {showDetails &&
-                  index === i &&
-                  d.entity &&
-                  d.entity.student_roll_states.map((en, i) => {
-                    return (
-                      <S.CardDetailsContainer>
-                        <S.CardItem>
-                          <S.CardItemSpan>Student id: &nbsp;</S.CardItemSpan>
-                          {en.student_id}
-                        </S.CardItem>
-                        <S.CardItem>
-                          <S.CardItemSpan>Roll state: &nbsp;</S.CardItemSpan>
-                          {en.roll_state}
-                        </S.CardItem>
-                      </S.CardDetailsContainer>
-                    )
-                  })}
-              </S.Card>
-            )
-          })}
-        </S.CardContainer>
+                  <S.CardItem>
+                    <S.CardItemSpan>Details: &nbsp;</S.CardItemSpan>
+                    <S.CardIconContainer>
+                      <FontAwesomeIcon onClick={() => handleDetails(i)} icon={faPlay} />
+                    </S.CardIconContainer>
+                    {showDetails && index === i && (
+                      <>
+                        <S.EntityContainer>
+                          <S.CardItemSpan>Name: &nbsp;</S.CardItemSpan> {d.entity.name}
+                        </S.EntityContainer>
+                        <S.EntityContainer>
+                          <S.CardItemSpan>Completed at: &nbsp;</S.CardItemSpan> {formatDate(d.entity.completed_at)}
+                        </S.EntityContainer>
+                      </>
+                    )}
+                  </S.CardItem>
+                  {showDetails &&
+                    index === i &&
+                    d.entity &&
+                    d.entity.student_roll_states.map((en, ind) => {
+                      return (
+                        <S.CardDetailsContainer key={ind}>
+                          <S.CardItem>
+                            <S.CardItemSpan>Student id: &nbsp;</S.CardItemSpan>
+                            {en.student_id}
+                          </S.CardItem>
+                          <S.CardItem>
+                            <S.CardItemSpan>Roll state: &nbsp;</S.CardItemSpan>
+                            {en.roll_state}
+                          </S.CardItem>
+                        </S.CardDetailsContainer>
+                      )
+                    })}
+                </S.Card>
+              )
+            })}
+          </S.CardContainer>
+        )}
       </S.Container>
     </>
   )
